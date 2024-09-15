ef16710e7e98c9744795f57b6f92ef07a6afcc64
A L Hari Shekhar <84981622+harishekhar06@users.noreply.github.com>
2024-09-15 16:47:08 +0530

    react import

--- App.js
+++ App.js
@@ -1,19 +1,16 @@
-// const heading = React.createElement("h1", {}, "Hello from React");
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
-const parent = React.createElement("div", {id:"parent" },
-    React.createElement("div",
-        { id:"child1" },[
+import React from "react";
+import ReactDOM from "react-dom";
+const parent = React.createElement("div", {id:"parent" },[
+    React.createElement("div",{id:"child"},[
         React.createElement("h1", {},"I am an h1 tag"),
         React.createElement("h2", {},"I am an h2 tag")
     ]),
-    React.createElement("div",
-        { id:"child2" },[
+    React.createElement("div",{ id:"child2" },[
         React.createElement("h1", {},"I am an h1 tag"),
         React.createElement("h2", {},"I am an h2 tag")
     ]),
 
-);
+]);
 console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
