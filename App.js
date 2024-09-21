8c8fd0467df4435415c34bbb800f1c81449800ef
A L Hari Shekhar <84981622+harishekhar06@users.noreply.github.com>
2024-09-21 23:25:28 +0530

    ..

--- App.js
+++ App.js
@@ -1,11 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-const heading = React.createElement(
-    "h1",
-    {id: "heading"},
-    "Namaskaaram Mavane"
-);
-console.log(heading);
+
 
 const jsxHeading = <h1 id="heading">Namaskaaram</h1>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
