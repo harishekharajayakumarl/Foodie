dd961b3f4d5c69f59c3199530bfb6f53f9725acf
A L Hari Shekhar <84981622+harishekhar06@users.noreply.github.com>
2024-09-22 11:51:43 +0530

    JSX codes

--- App.js
+++ App.js
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 
-const jsxHeading = <h1 id="heading">Namaskaaram</h1>;
+const jsxHeading = <h1 className="head">Namaskaaram</h1>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(jsxHeading);
