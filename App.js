fd0f89f00e0588e8287ae520dcb998be595db329
A L Hari Shekhar <84981622+harishekhar06@users.noreply.github.com>
2024-09-22 13:32:53 +0530

    React componenet+element

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// React element
 const heading = (
     <h1 className="head" tabIndex="5">
         Namaskaaram
     </h1>
 );
-
+// React functiinal component
+const HeadingComponent = () =>{
+    return <h1 className="heading">Namaskaram. This is Recat functional component</h1>;
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponent />);
