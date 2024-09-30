6c87bdd720055c8abbfd5a4c31d405052b7955ad
A L Hari Shekhar <84981622+harishekhar06@users.noreply.github.com>
2024-09-30 12:46:06 +0530

    Added restaurant card

--- App.js
+++ App.js
@@ -19,10 +19,33 @@ const Header = () => {
         </div>
     );
 };
+
+const RestaurantCard = () => {
+    return(
+        <div className="rest-card" style={{ backgroundColor: "#f0f0f0"}}>
+            <img className="rest-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/kewshvgvu2j4w0u9amjy" alt="food" />
+            <h3>Hotel Royale park</h3>
+            <h4>Indian, Chinese, Continental</h4>
+            <h4>4.1 stars</h4>
+            <h4>38 mins</h4>
+        </div>
+    );
+}
+const Body = () => {
+    return(
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="rest-container">
+                <RestaurantCard />
+            </div>
+        </div>
+    );
+}
 const AppLayout = () => {
     return(
         <div className="app">
             <Header />
+            <Body />
 
         </div>
     )
