60579103654e48d2f3efc44bf88bbaeff6305561
A L Hari Shekhar <84981622+harishekhar06@users.noreply.github.com>
2024-09-30 10:17:17 +0530

    Start of food delivery app

--- App.js
+++ App.js
@@ -1,18 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Title = () => (
-    <h1 className="head" tabIndex={"5"}>
-        Namaskaaram 
-    </h1>
-);
 
-// React Component
-const HeadingComponenet = () => (
-    <div id="container">
-        <Title />
-        <h1 className="heading">React Functional Component</h1>
-    </div>
-);
+const Header = () => {
+    return(
+        <div className="header">
+            <div className="logo-container">
+            <img className="logo" src="https://png.pngtree.com/png-vector/20221231/ourmid/pngtree-food-delivery-logo-template-design-sign-menu-vector-png-image_43664212.jpg"/>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+const AppLayout = () => {
+    return(
+        <div className="app">
+            <Header />
+
+        </div>
+    )
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponenet />);
+root.render(<AppLayout />);
