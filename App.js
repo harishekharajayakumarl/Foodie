89c3848aa384c2a088ff8469cd752f9b014b5d07
A L Hari Shekhar <84981622+harishekhar06@users.noreply.github.com>
2024-10-08 13:20:09 +0530

    lots

--- App.js
+++ App.js
@@ -22,17 +22,25 @@ const Header = () => {
 
 const RestaurantCard = (props) => {
     const { restData } = props;
+    const { 
+        cloudinaryImageId,
+        name, 
+        cuisines, 
+        avgRating, 
+        costForTwo,
+    } = restData?.info;
     return(
         <div className="rest-card" style={{ backgroundColor: "#f0f0f0"}}>
             <img className="rest-logo" 
             src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+
-                restData.info.cloudinaryImageId}
+                cloudinaryImageId}
             alt="food" 
             />
-            <h3>{restData.info.name}</h3>
-            <h4>{restData.info.cuisines}</h4>
-            <h4>{restData.info.avgRating}</h4>
-            <h4>{restData.info.deliveryTime}</h4>
+            <h3>{name}</h3>
+            <h4>{cuisines.join(", ")}</h4>
+            <h4>{avgRating} ⭐</h4>
+            <h4>{costForTwo}</h4>
+            <h4>{restData.info.sla.deliveryTime} minutes</h4>
         </div>
     );
 }
@@ -1288,16 +1296,10 @@ const Body = () => {
         <div className="body">
             <div className="search">Search</div>
             <div className="rest-container">
-                <RestaurantCard restData={resList[0]}/>
-                <RestaurantCard restData={resList[1]}/>
-                <RestaurantCard restData={resList[2]}/>
-                <RestaurantCard restData={resList[3]}/>
-                <RestaurantCard restData={resList[4]}/>
-                <RestaurantCard restData={resList[5]}/>
-                <RestaurantCard restData={resList[6]}/>
-                <RestaurantCard restData={resList[7]}/>
-                <RestaurantCard restData={resList[8]}/>
-
+                {
+                    resList.map(restaurant => (
+                    <RestaurantCard key={restaurant.info.id} restData={restaurant}/>))
+                }
             </div>
         </div>
     );
