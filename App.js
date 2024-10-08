fc2e90737b303d1da0352c6bda56797909febf06
A L Hari Shekhar <84981622+harishekhar06@users.noreply.github.com>
2024-10-08 11:27:01 +0530

    props

--- App.js
+++ App.js
@@ -20,23 +20,38 @@ const Header = () => {
     );
 };
 
-const RestaurantCard = () => {
+const RestaurantCard = (props) => {
+    const { restData } = props;
     return(
         <div className="rest-card" style={{ backgroundColor: "#f0f0f0"}}>
             <img className="rest-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/kewshvgvu2j4w0u9amjy" alt="food" />
-            <h3>Hotel Royale park</h3>
-            <h4>Indian, Chinese, Continental</h4>
-            <h4>4.1 stars</h4>
-            <h4>38 mins</h4>
+            <h3>{props.resName}</h3>
+            <h4>{props.cuisine}</h4>
+            <h4>{props.stars}</h4>
+            <h4>{props.time}</h4>
         </div>
     );
 }
+
+    
 const Body = () => {
     return(
         <div className="body">
             <div className="search">Search</div>
             <div className="rest-container">
-                <RestaurantCard />
+                <RestaurantCard 
+                resName="Royale Park" 
+                cuisine="India, Chinese, Continental"
+                stars = "stars: 4.1"
+                time="40 minutes"
+                />
+                <RestaurantCard 
+                resName="KFC" 
+                cuisine="Fried items"
+                stars = "stars: 4.5"
+                time="30 minutes"
+                />
+
             </div>
         </div>
     );
