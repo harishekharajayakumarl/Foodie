9c5ad5e6f9410f3e31d3194ce1d7918d35cece6d
A L Hari Shekhar <84981622+harishekhar06@users.noreply.github.com>
2024-10-09 16:34:59 +0530

    css changes

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,16 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../../utils/mockData";
 
+const listOfRestaurants = [];
 const Body = () => {
     return(
         <div className="body">
-            <div className="search">Search</div>
+            <div className="filter">
+                <button className="filter-btn" onClick={()=>{console.log("Button Clicked")}}>Top Rated Restaurants</button>
+            </div>
             <div className="rest-container">
                 {
-                    resList.map(restaurant => ( //written index for reference to the below point
+                    listOfRestaurants.map(restaurant => ( //written index for reference to the below point
                     <RestaurantCard key={restaurant.info.id} restData={restaurant}/>)) //good convention: whenever doing a .map always put a key in the functional component below //good practise wrt React render cycle //important for optimization and efficiency
                     //never use key={index}, not recommended - bad practice as per React documentation ~ for more info https://react.dev/learn/rendering-lists, https://robinpokorny.com/blog/index-as-a-key-is-an-anti-pattern/, https://robinpokorny.medium.com/index-as-a-key-is-an-anti-pattern-e0349aece318
 //unique id as key (BEST option) index as key (can be used but not recommended) not using key (NOT acceptable)  
