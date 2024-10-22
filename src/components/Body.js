14c266ecaeddb20c50061bb3804b5e5201842c22
A L Hari Shekhar <84981622+harishekhar06@users.noreply.github.com>
2024-10-22 10:16:32 +0530

    Body

--- src/components/Body.js
+++ src/components/Body.js
@@ -12,9 +12,14 @@ const Body = () => {
     const [listOfRestaurants, setListOfRestaurants] = useState(resList);
 // The second parameter here (setListOfrestaurant) is the function for any modification or any other fuctionalities
     useEffect(() => {
-        console.log("useEffect Called");
+        fetchData();
     }, [])
 
+    const fetchData = async () => {
+        const data = await fetch(
+            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+        );
+    }
     console.log('Body rendered');
     return(
         <div className="body">
