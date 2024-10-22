30dbbdd0f5401e798ee3c7d3451091b0c8b2c601
A L Hari Shekhar <84981622+harishekhar06@users.noreply.github.com>
2024-10-22 11:00:00 +0530

    Body component

--- src/components/Body.js
+++ src/components/Body.js
@@ -19,6 +19,9 @@ const Body = () => {
         const data = await fetch(
             "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
         );
+        const json = await data.json();
+        console.log(json);
+        setListOfRestaurants(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
     }
     console.log('Body rendered');
     return(
