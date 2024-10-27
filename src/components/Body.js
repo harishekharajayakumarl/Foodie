2ff4721698353d47b294e67fd89650a228264d23
A L Hari Shekhar <84981622+harishekhar06@users.noreply.github.com>
2024-10-27 16:46:14 +0530

    Body

--- src/components/Body.js
+++ src/components/Body.js
@@ -23,7 +23,7 @@ const Body = () => {
 
     const fetchData = async () => {
         const data = await fetch(
-            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=8.558095699999999&lng=76.8807437&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
         );
         const json = await data.json();
         console.log(json);
