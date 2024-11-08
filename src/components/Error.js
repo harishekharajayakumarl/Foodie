ca753fd69fbeface8b37e5f44dab0dc4c1b22d98
A L Hari Shekhar <84981622+harishekhar06@users.noreply.github.com>
2024-11-08 17:25:13 +0530

    Error component

--- src/components/Error.js
+++ src/components/Error.js
@@ -5,7 +5,9 @@ const Error = () => {
     console.log(err);
     return(
         <div>
-            <h1>OOPS, Something went Wrong!!</h1>
+            <h1>OOPS!!!</h1>
+            <h2>Something went Wrong!!</h2>
+            <h3>{err.status} : {err.statusText}</h3>
         </div>
     );
 };
