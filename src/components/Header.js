50dbf8f3c82dd3e2bc9e690bf26a9dc0a59efc8c
A L Hari Shekhar <84981622+harishekhar06@users.noreply.github.com>
2024-11-14 11:13:48 +0530

    Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import { LOGO_URL } from "../../utils/constants";
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
     const [btnNameReact, setBtnNamereact] = useState("Login");
@@ -19,10 +20,10 @@ const Header = () => {
             </div>
             <div className="nav-items">
                 <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact Us</li>
-                    <li>Cart</li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/contact">Contact Us</Link></li>
+                    <li><Link to="/cart">Cart</Link></li>
                     <button className="login" 
                     onClick={() => {
                         btnNameReact === "Login"
