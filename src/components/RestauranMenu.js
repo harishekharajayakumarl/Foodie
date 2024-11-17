d24ef388466f175370ea06583859bb41fdc08ac0
A L Hari Shekhar <84981622+harishekhar06@users.noreply.github.com>
2024-11-17 12:51:46 +0530

    Restaurant menu card

---
+++ src/components/RestauranMenu.js
@@ -0,0 +1,14 @@
+const RestaurantMenu = () => {
+    return(
+        <div className="menu">
+            <h1>Name of the restaurant</h1>
+            <h3>Menu</h3>
+            <ul>
+                <li>Biriryani</li>
+                <li>Dosa</li>
+                <li>Porotta + Beef</li>
+            </ul>
+        </div>
+    );
+};
+export default RestaurantMenu;
