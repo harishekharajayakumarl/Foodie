f511b8dcd9769ac9973b681daab7053d3d583dde
A L Hari Shekhar <84981622+harishekhar06@users.noreply.github.com>
2024-11-24 11:50:28 +0530

    Menu card

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
+import { MENU_API } from "../../utils/constants";
+
 const RestaurantMenu = () => {
     useEffect(()=>{
         fetchMenu();
@@ -9,9 +11,7 @@ const RestaurantMenu = () => {
     const { resId } = useParams();
     
     const fetchMenu = async ()     => {
-        const data = await fetch(
-            "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=8.558095699999999&lng=76.8807437&restaurantId=113324&catalog_qa=undefined&submitAction=ENTER"
-        );
+        const data = await fetch(MENU_API + resId);
         const json = await data.json();
         console.log(json);
         setResInfo(json.data)
@@ -30,7 +30,7 @@ const RestaurantMenu = () => {
             <h2>Menu</h2>
             <ul>
                 {itemCards.map((item) => (
-                    <li>{item.card.info.name} - { "Rs." }{item.card.info.price/100 || item.card.info.defaultPrice/100}</li>
+                    <li key={item.card.info.id}>{item.card.info.name} - { "Rs." }{item.card.info.price/100 || item.card.info.defaultPrice/100}</li>
                 ))}
             </ul>
         </div>
