8c07746a5c6e9fe9ea3c925e9ec89fb94be5fce3
A L Hari Shekhar <84981622+harishekhar06@users.noreply.github.com>
2024-12-03 10:28:51 +0530

    Class based components - componentDidMount

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,33 @@
+import { Component } from "react";
 import User from "./User";
 import UserClass from "./userClass";
 
-const About = () => {
-    return(
-        <div>
-            <h1>About</h1>
-            {/* <User name="Harishekhar" location="Alappuzha"/> */}
-            <UserClass name="Hari Shekhar" location="Thiruvananthapuram"/>
-        </div>
-    );
-};
-
+// const About = () => {
+//     return(
+//         <div>
+//             <h1>About</h1>
+//             {/* <User name="Harishekhar" location="Alappuzha"/> */}
+//             <UserClass name="Hari Shekhar" location="Thiruvananthapuram"/>
+//         </div>
+//     );
+// };
+class About extends Component {
+    constructor(props){
+        super(props);
+        console.log("Parent Constructor");
+    }
+    componentDidMount(){
+        console.log("Parent DidMount");
+    }
+    render(){
+        console.log("Parent Render");
+        return(
+                    <div>
+                        <h1>About</h1>
+                        {/* <User name="Harishekhar" location="Alappuzha"/> */}
+                        <UserClass name="Hari Shekhar" location="Thiruvananthapuram"/>
+                    </div>
+                );
+    }
+}
 export default About;
