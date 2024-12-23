3d878f38e5ba92165e9dd833b0f337ecfa902aad
harishekharajayakumarl <harishekhar006@gmail.com>
2024-12-23 10:38:27 +0530

    Custom hooks

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,6 +2,7 @@ import RestaurantCard from "./RestaurantCard";
 import { useState, useEffect } from "react"; 
 import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
+import useOnlineStatus  from "../../utils/useOnlineStatus";
 
 const Body = () => {
 
@@ -31,6 +32,14 @@ const Body = () => {
         setListOfRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
         setFilteredrestaurant(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
     };
+    
+    const onlineStatus = useOnlineStatus()
+    if(onlineStatus === false) 
+        return (
+        <h1>
+            Looks like you are offline!!! Please check your internet connection
+        </h1>
+        );
 
     // Conditional Rendering
     // if(listOfRestaurants.length === 0){
