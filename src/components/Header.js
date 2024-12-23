80090b52b4748ee54dbb49703542a86389ec19fc
harishekharajayakumarl <harishekhar006@gmail.com>
2024-12-23 10:45:27 +0530

    Custom hooks

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import { LOGO_URL } from "../../utils/constants";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../../utils/useOnlineStatus";
 
 const Header = () => {
     const [btnNameReact, setBtnNamereact] = useState("Login");
+    const onlineStatus = useOnlineStatus();
     // If no dependency array, useEffect is called on every render
     // If there is an empty dependency array, useEffect will be rendered only on the initial render and only once
     useEffect(() => {
@@ -20,6 +22,7 @@ const Header = () => {
             </div>
             <div className="nav-items">
                 <ul>
+                    <li>Online Status: {onlineStatus ? "✅": "🔴"}</li>
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/about">About</Link></li>
                     <li><Link to="/contact">Contact Us</Link></li>
