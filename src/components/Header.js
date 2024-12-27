9526ecc7c1337a6ef0d63331864009875bf5a495
harishekharajayakumarl <harishekhar006@gmail.com>
2024-12-27 15:50:29 +0530

    LAZY LOADING AND SUSPENSE

--- src/components/Header.js
+++ src/components/Header.js
@@ -26,6 +26,7 @@ const Header = () => {
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/about">About</Link></li>
                     <li><Link to="/contact">Contact Us</Link></li>
+                    <li><Link to="/grocery">Grocery</Link></li>
                     <li><Link to="/cart">Cart</Link></li>
                     <button className="login" 
                     onClick={() => {
