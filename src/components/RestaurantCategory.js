7f5ec611bfb2662e11164122608719d89752aef4
harishekharajayakumarl <harishekhar006@gmail.com>
2025-03-25 11:46:20 +0530

    Restaurant category accordion

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -2,7 +2,7 @@ const RestaurantCategory = (props) => {
     return(
         <div>
             <div className="w-6/12 bg-gray-50 shadow-lg p-4 mx-auto my-4 flex justify-between">
-                <span className="font-bold text-lg">{props.data.title}</span>
+                <span className="font-bold text-lg">{props.data.title} ({props.data.itemCards.length})</span>
                 <span>🔽</span>
             </div>
         </div>
