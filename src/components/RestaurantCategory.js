10e115ea38515aa0396f7f87e11ddae23578178a
harishekharajayakumarl <harishekhar006@gmail.com>
2025-03-25 11:43:10 +0530

    Restaurant category accordion

---
+++ src/components/RestaurantCategory.js
@@ -0,0 +1,11 @@
+const RestaurantCategory = (props) => {
+    return(
+        <div>
+            <div className="w-6/12 bg-gray-50 shadow-lg p-4 mx-auto my-4 flex justify-between">
+                <span className="font-bold text-lg">{props.data.title}</span>
+                <span>🔽</span>
+            </div>
+        </div>
+    );
+};
+export default RestaurantCategory;
--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,6 +1,7 @@
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
 import useRestaurantMenu from "../../utils/useRestaurantMenu";
+import RestaurantCategory from "./RestaurantCategory";
 
 const RestaurantMenu = () => {
     const { resId } = useParams();
@@ -11,19 +12,17 @@ const RestaurantMenu = () => {
 
     const { name, cuisines, costForTwoMessage, totalRatingsString, avgRatingString, avgRating  } = resInfo?.cards[2]?.card?.card?.info || {};
     const { itemCards } = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card || {};
+    const categories = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter((element) => element?.card?.["card"]?.["@type"] === "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory");
+    console.log(categories);
     
     return (
-        <div className="m-4 p-4 rounded-lg shadow-lg bg-green-50" >
-            <h1 className="font-bold text-xl text-center">{name}</h1>
-            <h2  className="font-bold text-lg flex wrap" style={{ display: 'inline-block'}}>{avgRatingString} ({totalRatingsString})</h2>
-            <h2 className='font-bold text-lg'>{costForTwoMessage}</h2>
-            <p>{cuisines.join(", ")}</p> 
-            <h2 className="font-bold text-xl">Menu</h2>
-            <ul>
-                {itemCards.map((item) => (
-                    <li key={item.card.info.id}>{item.card.info.name} - ₹{item.card.info.price/100 || item.card.info.defaultPrice/100}</li>
-                ))}
-            </ul>
+        <div className="text-center" >
+            <h1 className="font-bold my-6 text-2xl">{name}</h1>
+            {/* <h2  className="font-bold text-lg flex wrap" style={{ display: 'inline-block'}}>{avgRatingString} ({totalRatingsString})</h2> */}
+            <p className='font-bold text-lg'>{cuisines.join(", ")} - {costForTwoMessage}</p> 
+            {categories?.map((category, index) => (
+                <RestaurantCategory data={category?.card?.card}/>
+      ))}
         </div>
     );
 };
