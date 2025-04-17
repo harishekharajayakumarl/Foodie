326709f1c0e94f7964f9b0210f4ee16c1516df8c
harishekharajayakumarl <harishekhar006@gmail.com>
2025-04-17 11:19:08 +0530

    Lifting the state up in react

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -2,14 +2,15 @@ import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
 import useRestaurantMenu from "../../utils/useRestaurantMenu";
 import RestaurantCategory from "./RestaurantCategory";
+import { useState } from "react";
 
 const RestaurantMenu = () => {
     const { resId } = useParams();
     const resInfo = useRestaurantMenu(resId);
+    const [showIndex, setShowIndex] = useState(null);
 
     if (resInfo === null) return <Shimmer />;
-
-
+    
     const { name, cuisines, costForTwoMessage, totalRatingsString, avgRatingString, avgRating  } = resInfo?.cards[2]?.card?.card?.info || {};
     const { itemCards } = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card || {};
     const categories = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter((element) => element?.card?.["card"]?.["@type"] === "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory");
@@ -21,7 +22,10 @@ const RestaurantMenu = () => {
             {/* <h2  className="font-bold text-lg flex wrap" style={{ display: 'inline-block'}}>{avgRatingString} ({totalRatingsString})</h2> */}
             <p className='font-bold text-lg'>{cuisines.join(", ")} - {costForTwoMessage}</p> 
             {categories?.map((category, index) => (
-                <RestaurantCategory key={category?.card?.card?.title} data={category?.card?.card}/>
+                <RestaurantCategory key={category?.card?.card?.title} 
+                data={category?.card?.card} 
+                showItems={(index === showIndex ? true : false)} 
+                setShowIndex={() => setShowIndex(index === showIndex ? null : index)}/>
       ))}
         </div>
     );
