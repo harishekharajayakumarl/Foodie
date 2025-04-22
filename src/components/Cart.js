8b6e51a8fae6bbbf7564f4b494e2d99d705a9f6b
harishekharajayakumarl <harishekhar006@gmail.com>
2025-04-22 16:01:03 +0530

    React-Redux-store-slice

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,8 +1,31 @@
+import { useDispatch, useSelector } from "react-redux";
+import ItemList from "./ItemList";
+import { clearCart } from "../../utils/cartSlice";
+import cart from "../components/images/cart.png";
+
 const Cart = () => {
+    const dispatch = useDispatch();
+    const handleClearCart = () => {
+        dispatch(clearCart());
+    }
+    const cartItems = useSelector((store) => store.cart.items);
+
     return (
         <div className="text-center m-4 p-4">
-            <h1 className="text-2xl font-bold">Cart</h1>
+            <div className="text-right">
+                <button className="m-2 p-2 bg-black text-white rounded-lg" onClick={handleClearCart}>Clear Cart</button>
+            </div>
+            <div className="w-6/12 m-auto">
+                {cartItems.length === 0 && (
+                    <>
+                        <img src={cart} alt="Empty Cart" className="mx-auto w-48 h-48 mb-4" />
+                        <h1>Your cart is empty. Add items to cart!</h1>
+                    </>
+                )}
+                <ItemList items={cartItems} />
+            </div>
         </div>
     );
 };
+
 export default Cart;
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,40 +1,59 @@
 import { CDN_URL } from "../../utils/constants";
 import { useDispatch } from "react-redux";
+import { useCallback } from "react";
 import { addItem } from "../../utils/cartSlice";
 
 const ItemList = ({ items }) => {
     const dispatch = useDispatch();
-    const handelAddItem = () => {
-        // dispatch an action
-        dispatch(addItem());
-    };
-    return(
+    const handleAddItem = useCallback(
+        (item) => {
+            dispatch(addItem(item));
+        },
+        [dispatch]
+    );
+
+    return (
         <div>
-            {items.map((item) =>(
-                <div key={item.card.info.id} className="p-2 m-2 border-gray-200 border-b-2 text-left flex justify-between">
-                    <div className="w-9/12">
-                        <div className="py-2 font-bold">
+            {items && items.map((item) => (
+                item && item.card && item.card.info ? (
+                    <div
+                        key={item.card.info.id}
+                        className="p-2 m-2 border-gray-200 border-b-2 text-left flex justify-between"
+                    >
+                        <div className="w-9/12">
+                            <div className="py-2 font-bold">
                                 <span>{item.card.info.name}</span>
                             </div>
                             <div className="py-2 font-semibold">
-                                <span> ₹{item.card.info.price ?
-                                item.card.info.price / 100
-                                :
-                                item.card.info.defaultPrice / 100}
+                                <span>
+                                    ₹{item.card.info.price
+                                        ? item.card.info.price / 100
+                                        : item.card.info.defaultPrice / 100}
                                 </span>
                             </div>
                             <p className="text-xs">{item.card.info.description}</p>
-                    </div>
-                    <div className="w-3/12 p-4 flex flex-col justify-end items-center">
-                        <div className="absolute"> 
-                        <button className="p-2 mx-25 rounded-lg bg-black text-white shadow-lg" 
-                        onClick={handelAddItem}> Add +</button>
                         </div>
-                        <img src={CDN_URL + item.card.info.imageId} /> 
+                        <div className="w-3/12 p-4 flex flex-col justify-end items-center">
+                            <div className="absolute">
+                                <button
+                                    className="p-2 mx-25 rounded-lg bg-black text-white shadow-lg"
+                                    onClick={() => handleAddItem(item)}
+                                >
+                                    Add +
+                                </button>
+                            </div>
+                            {item.card.info.imageId && (
+                                <img
+                                    src={CDN_URL + item.card.info.imageId}
+                                    alt={item.card.info.name}
+                                />
+                            )}
+                        </div>
                     </div>
-                </div>
+                ) : null
             ))}
         </div>
-    )
+    );
 };
+
 export default ItemList;
