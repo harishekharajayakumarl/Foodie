2956da931fb4d4cf7c927e554b67381e9f63576c
harishekharajayakumarl <harishekhar006@gmail.com>
2025-04-22 16:27:03 +0530

    React-Redux-store-slice

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -9,6 +9,11 @@ const Cart = () => {
         dispatch(clearCart());
     }
     const cartItems = useSelector((store) => store.cart.items);
+    const totalPrice = cartItems.reduce((acc, item) => {
+        const price = item.card.info.hasOwnProperty('defaultPrice') ?
+          item.card.info.defaultPrice : item.card.info.price;
+        return acc + price;
+      }, 0);
 
     return (
         <div className="text-center m-4 p-4">
@@ -19,11 +24,12 @@ const Cart = () => {
                 {cartItems.length === 0 && (
                     <>
                         <img src={cart} alt="Empty Cart" className="mx-auto w-48 h-48 mb-4" />
-                        <h1>Your cart is empty. Add items to cart!</h1>
+                        <h1 className="m-4 text-2xl font-semibold">Your cart is empty. Add items to cart!</h1>
                     </>
                 )}
                 <ItemList items={cartItems} />
             </div>
+            {cartItems.length > 0 && <h1 className="w-6/12 m-auto text-right text-2xl font-bold pt-5">Total Bill: ₹{(totalPrice / 100).toFixed(2)}</h1>}
         </div>
     );
 };
